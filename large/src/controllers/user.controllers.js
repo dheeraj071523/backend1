a97ed433706f2423f63580e81d20e3196b4be49e
Dheeraj kumawat <dheerajkumawat0707@gmail.com>
2024-03-11 22:01:32 +0530

    add register

--- large/src/controllers/user.controllers.js
+++ large/src/controllers/user.controllers.js
@@ -1,10 +1,80 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudnary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // ye ek high order function hai
-  res.status(200).json({
-    message: "ok",
+
+  //step of the register route  // logic buliding
+  // get user details from fronted
+  // vaildation - not empty
+  // check if the user already exists: username, email
+  // upload them cloudinary,avater
+  // create user object --create entry in db
+  // remove password and refresh token field from response
+  //  check for user creation
+  // return res
+
+  const { fullname, email, username, password } = req.body;
+  // desctruing of the data here we take the data postman or req to particular object each value data
+  console.log("email", email);
+
+  // if (fullname === "") {
+  //   throw new ApiError(400, "fullname is required")
+  // }
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already existed");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path; // req.files this access give the multer // ? this is called optionaly // [0] give the object  // here wwe access the file
+  //  here the file storage in the local storage
+
+  const coverImageLocalPath = req.files?.coverImage[0]?.path; // here we take the coverImage file access
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatarn file is required");
+  }
+
+  const user = await User.create({
+    fullname,
+    avatar: avatar_url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refresh Token"
+  ); // see the user is actully created or not this _id give the  mongoDB
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registring a user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registerd succesfully"));
 });
 
 export { registerUser };
--- large/src/routes/user.routes.js
+++ large/src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser); // yaha par aaye ga app .use se vo ek middleware hai vo hamashe  hi same hi rahe ga
+router.route("/register").post(
+  upload.fields([
+    // apan ne array islye nahi liya kyuki array ek hi filed mai multiiple value leta hai to apan ko multiple object vaalue cahyie to apn ne fields liya hai
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+
+  registerUser
+); // yaha par aaye ga app .use se vo ek middleware hai vo hamashe  hi same hi rahe ga
 
 export default router;
