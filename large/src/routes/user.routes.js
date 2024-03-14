2c772d5960bdd052b17e0092356b2209470c6bed
Dheeraj kumawat <dheerajkumawat0707@gmail.com>
2024-03-15 00:51:32 +0530

    aggreigate

--- large/src/routes/user.routes.js
+++ large/src/routes/user.routes.js
@@ -54,7 +54,7 @@ router
 
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile); // params use ho raha hai
 
